540d04513e18a16de157a8e3b10de4f3d095f1c7
Matheus Canuto <70597769+MatheusCanuto07@users.noreply.github.com>
2023-12-14 16:39:57 -0300

    Features propostas finalizadas

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+
+namespace Apagar_arquivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destino = @"C:\Users\Matheus Canuto\Desktop\Destino";
+            int nFoto = 0;
+            for(int i = 100; i < 389; i++)
+            {
+                string sourcePath = @"C:\Users\Matheus Canuto\Desktop\Fotos ipad\DCIM\"+i+"APPLE";
+                int j = i;
+                foreach (string fotos in Directory.GetFiles(sourcePath))
+                {
+                    nFoto += 1;
+                    Console.WriteLine(fotos);
+                    File.Move(fotos, @"C:\Users\Matheus Canuto\Desktop\Destino\FotoMarcia"+ nFoto + " .JPG");
+                }
+            }
+
+        }
+    }
+}
